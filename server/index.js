65d7daef5acc5ae26a32210f095db93e00b045b3
talitman <talitmandotan@gmail.com>
2022-08-21 18:45:21 +0300

    fake auth with json data on the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
+const user1 = require("./fakeDB/users/user1.json");
 
 app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/views/welcome.html");
+  res.redirect(
+    `/welcome?email=${req.query.email}&password=${req.query.password}`
+  );
+});
+
+app.get("/welcome", (req, res) => {
+  const email = user1.email;
+  const password = user1.password;
+  if (req.query.email === email && req.query.password === password) {
+    res.sendFile(__dirname + "/views/welcome.html");
+  } else {
+    res.send("Invalid email or password");
+  }
 });
 
 app.listen(3000);
